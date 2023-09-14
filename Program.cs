562b07f4f11263f3b230687ce2064c6c08345a14
vboxuser <vboxuser@WINDOWS10-64>
2023-09-14 16:33:53 +0200

    Enunciado

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
-﻿namespace Libreria
+﻿/*
+Se desea diseñar un programa que registre libros (técnicos y novelas) para una librería y permita buscarlos, venderlos y verificar su stock. 
+Las novelas se clasifican como de ciencia ficción, romance misterio, juveniles y policiales. Los libros técnicos se clasifican como de ingeniería, ciencias naturales o ciencias sociales. 
+Cada libro tiene un título, uno o más autores, una editorial, un año de edición y formato (tapa dura o blanda) . Los libros tienen además un código OSBN y capítulos. 
+La librería obtiene los libros por medio de proveedores que representan a una o más editoriales. De cada libro tenemos un stock (puede ser 0). Al venderse el libro, se actualiza. Si un cliente requiere un libro cuyo stock es 0, se puede hacer un encargo.  
+*/
+
+namespace Libreria
 {
     public class GestionLibreria 
     {
